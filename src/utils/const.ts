196bcdb55ae04449099e5237c1a04c47f97667d3
delta-09 <623667953@qq.com>
2026-04-05 21:19:46 +0800

    Update const.ts

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -31,9 +31,9 @@ const USE_GOOGLE_ANALYTICS = false;
 const GOOGLE_ANALYTICS_TRACKING_ID = '';
 
 // styling: set to `true` if you want dash-line route
-const USE_DASH_LINE = true;
+const USE_DASH_LINE = false;
 // styling: route line opacity: [0, 1]
-const LINE_OPACITY = 0.4;
+const LINE_OPACITY = 1;
 // styling: map height - responsive design
 // Use smaller height on mobile devices for better user experience
 const MAP_HEIGHT = window.innerWidth <= 768 ? 250 : 600;
